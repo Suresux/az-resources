ea7b5a886027d63f39663aee465d0a1cb262f7aa
Suresux <100307594+Suresux@users.noreply.github.com>
2026-01-07 18:17:17 +0530

    Update variables.tf

--- webapp/variables.tf
+++ webapp/variables.tf
@@ -22,25 +22,3 @@ variable "tags" {
   type    = map(string)
   default = {}
 }
-
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Tenant ID of Azure AD"
-  type        = string
-}
-
-variable "client_id" {
-  description = "Service Principal client ID"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Service Principal secret"
-  type        = string
-  sensitive   = true
-}
